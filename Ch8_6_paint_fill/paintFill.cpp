712e19d7b4a66850d1498bf1e9dd62828bc2e024
larrycheungbai <cszhanglei@gmail.com>
2017-07-18 16:41:56 -0700

    comment here

--- Ch8_6_paint_fill/paintFill.cpp
+++ Ch8_6_paint_fill/paintFill.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-enum Colors 
+enum Color 
 {
 	Black,
 	White,
@@ -13,9 +13,37 @@ enum Colors
 };
 
 
-int main()
+bool paintFill(vector< vector<Color> > screen, int x, int y, Color newColor)
 {
 
+	if (x<0 || x > (screen.size()-1)) 
+	{
+		cout<<"x out of range" <<endl;
+		return false;
+	}
+	if(y<0 || y >  (screen[0].size()-1))
+	{
+		cout<<"y out of range" <<endl;
+		return false
+	}
+	if(screen[x][y]==newColor)
+		return false;
+	if(screen[x][y]!=newColor)
+	{
+		paintFill( screen, x-1, y, newColor);
+		paintFill( screen, x+1, y, newColor);
+		paintFill( screen, x, y_1, newColor);
+		paintFill( screen, x, y+1, newColor);
+	}
+	return true;
+}
+
+int main()
+{
 	cout<<"This is the paintFill!"<<endl;
+	
+
+
+
 	return 0;
 }
